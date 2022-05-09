e2990676811aed2e063cb740abe3c90fa1ae4cdb
dedrie p <dedrie@dedries-MacBook-Air.local>
2022-05-09 01:49:24 -0400

    started service worker

---
+++ public/service-worker.js
@@ -0,0 +1,8 @@
+const   APP_PREFIX = 'Budget Tracker -'
+const VERSION = 'version_2.0'
+const CACHE_NAME = APP_PREFIX + VERSION
+const FILES_TO_CACHE = [
+ './index.html' , 
+ './js/index.js',
+  './css/style.css'
+]
