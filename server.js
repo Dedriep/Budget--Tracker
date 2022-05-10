57be12ed3153a35fba2c86e2485a2e4e9d8cc706
dedrie p <dedrie@dedries-MacBook-Air.local>
2022-05-10 02:39:42 -0400

    deploy

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:3000/Budget-Tracker', {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useUnifiedTopology: true
 });
 
 // routes
